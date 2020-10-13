03dfbbaf661e9566fe927f5d394b5321ff316458
tofunmikupoluyi <tofunmi@live.com>
2020-10-13 16:36:22 +0400

    added timing

--- analyzers/dynamic/browser.js
+++ analyzers/dynamic/browser.js
@@ -5,68 +5,107 @@
 'use strict';
 var webdriver = require("selenium-webdriver");
 var chrome = require('selenium-webdriver/chrome');
-var path = require('chromedriver').path;
-var service = new chrome.ServiceBuilder(path).build();
-chrome.setDefaultService(service);
-
 module.exports = function()
 {
 	this.driver = null;
 	this.min_required_time = 5000;
 
-	this.start = function()
+	this.start = async function()
 	{
-		this.driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome())
-			.build();
+		let directory = __dirname+"/chrome_extension.crx";
+		console.log(webdriver.Capabilities.chrome().map_);
+		try {
+			this.driver = await new webdriver.Builder()
+				.withCapabilities(webdriver.Capabilities.chrome())
+				.setChromeOptions(new chrome.Options().addExtensions([directory])) 
+				.setChromeService(new chrome.ServiceBuilder("/usr/bin/chromedriver"))
+				.build();
+		} catch (err) {
+			console.log(err);
+		}
 	};
 
+	this.clickElement = function(element)
+	{
+		return new Promise((resolve, reject) => {
+			element.id_
+				.then((res) => {
+					element.click();
+					resolve(element);
+				})
+				.catch((err) => {
+
+				});
+		})
+	}
+
+	this.waitForPageLoad = function()
+	{
+		return new Promise((resolve, reject) => {
+			setTimeout(()=>{
+				resolve(true);
+			}, 5000)
+		});
+	}
+
+	this.waitForClickResponse = function() {
+		return new Promise((resolve, reject) => {
+			setTimeout(()=>{
+				resolve(true);
+			}, 3000)
+		});
+	}
 
 	this.load = async function(url, timeout, success)
 	{
-		let me = this;
-
 		// Load the page.
-		this.driver.get("file://"+url);
+		await this.waitForPageLoad();
+		await this.driver.get("https://culinary-architecture.com");
+		await this.waitForPageLoad();
 
-		// Trigger click event for every element
+		// Simulate interactivity
 		try {
 			let allElements = await this.driver.executeScript("return Array.from(document.querySelectorAll('*'));");
-			allElements.forEach( (element) => {
-				element.id_
-				.then(()=>{
-					element.click();
-				})
-				.catch((err) => {
-				});
-			});
+			let numberOfElementsOnPage = allElements.length;
+
+			// Loop through all elements and click on them
+			for (let i = 0; i<numberOfElementsOnPage; i++) {
+				await this.clickElement(allElements[i]);
+				await this.waitForClickResponse();
+
+				// If there is a page change in response to a click, we want to go back to our original page
+				if (await this.driver.getCurrentUrl() != "https://culinary-architecture.com") {
+					console.log("Page change, returning to original page");
+					await this.driver.get("https://culinary-architecture.com");
+					await this.waitForPageLoad();
+
+					allElements = await this.driver.executeScript("return Array.from(document.querySelectorAll('*'));");
+					// Sanity check, if we don't have the same page structure, restart
+					if (numberOfElementsOnPage != allElements.length) {
+						i = 0;
+						numberOfElementsOnPage = allElements.length;
+					}
+				}
+			}
 		} catch (err) {
 			console.log(err);
 		}
 
-		// The function to run.
-		let runner = function()
-		{
-			let logs = [];
-			me.driver.manage().logs().get(webdriver.logging.Type.BROWSER).then(function(entries)
-			{
-				entries.forEach(function(entry)
-				{
-					logs.push(entry.message);
-				});
-				success(logs);
-			});
-		};
-
-		// After the timeout, collect the console.log entries and call the success callback function.
-		if(timeout)
-		{
-			// Wait at least min_required_time (browser startup time) seconds, more if we have a longer timeout.
-			setTimeout(runner, Math.max(timeout, me.min_required_time));
-		}else{
-			setTimeout(runner, me.min_required_time);
-		}
+		this.returnLogs(success);
 	};
 
+	this.returnLogs = function(success) {
+		let logs = [];
+		this.driver.manage().logs().get(webdriver.logging.Type.BROWSER).then(function(entries)
+		{
+			entries.forEach(function(entry)
+			{
+				logs.push(entry.message);
+			});
+			console.log(logs)
+			success(logs);
+		});
+	}
 
 	this.stop = function()
 	{
